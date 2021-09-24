f6d5ca008d5253d3274832e3d9556a57fb1f4252
Logan Martlew <logan.martlew@gmail.com>
2021-09-24 12:35:14 +1200

    Restructured layout component to use flexbox and grow on the main element

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,14 +3,20 @@ import styled from 'styled-components';
 import Header from '@components/Header';
 
 const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   min-height: 100vh;
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
 `;
 
 const Layout: FC = ({ children }) => {
   return (
     <LayoutWrapper>
       <Header />
-      <main>{children}</main>
+      <Main>{children}</Main>
     </LayoutWrapper>
   );
 };
